64347777a44525f48af1e5339b0c8ddefb3ccabd
Ilaria Orlando <ilaria.orlando1991@gmail.com>
2021-02-25 16:24:42 +0100

    add second api call using latitude and longitude from the first request

--- src/components/ApiCall.js
+++ src/components/ApiCall.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 //import Form from "./Form";
 
-const API_KEY = "6d62f680b47ba0a60f39206b9e1a714a";
+
 const axios = require('axios').default;
 
 const ApiCall = ({form}) => {
 
     const city = form.city
+    //const [longLat, setLongLat] = useState({long: "", lat: ""})
+
 
     const call = async () => {
 
 
-        const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&cnt=5&appid=${API_KEY}`
-        const request = axios.get(url)
-        const response = await request
+        const firsturl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&cnt=5&appid=${API_KEY}`
+        const firstrequest = axios.get(firsturl)
+        const firstresponse = await firstrequest
 
-        console.log(response)
+        const long = firstresponse.data.city.coord.lon
+        const lat = firstresponse.data.city.coord.lat
 
+
+        if(firstresponse){
+
+            callSecond(long, lat)
+
+        }
+
+
+    }
+
+    const callSecond = async (long, lat) =>{
+
+        const secondurl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=metric&exclude=hourly,minutely&appid=${API_KEY}`
+        const secondrequest = axios.get(secondurl)
+        const secondresponse = await secondrequest
+
+        console.log(secondresponse)
 
     }
 
